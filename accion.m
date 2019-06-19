ff3bdf501ce0a3e5d308b4953294cc72c39ade75
TesslaRay <cristian@MacBook-Pro-de-Cristian.local>
2019-06-19 02:18:53 -0400

    accion

---
+++ accion.m
@@ -0,0 +1,15 @@
+function M = accion(M, a)
+%   Input: Matriz de estado M y acción (entre 1 y 9)
+%   Output: Matriz de estado luego de la acción
+%   La acción 'realiza la jugada', ie agrega un 1 donde corresponde la
+%   acción
+    
+    for i = 1:3
+        for j = 1:3
+            if 3*j + i - 3 == a
+                M(j,i) = 1;
+            end
+        end
+    end
+    
+end
